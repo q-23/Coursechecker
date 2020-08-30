0233cea9d3204fef9003ebeff4e9b3712ade65fa
Aleksander Kwiatkowski <aleksander.kwiatkowski@gmail.com>
2020-08-30 17:21:09 +0200

    Fetching data from API, storing, simple table with mock data

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
-  <Navbar/>
+  <div>
+    <Navbar/>
+    <router-view/>
+  </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar";
+import getAll from "@/functions/getAll";
 export default {
   name: 'App',
-
   components: {
     Navbar,
   },
-
-  data: () => ({
-    //
-  }),
+  methods: {
+    fetchCurrenciesCourses: async function () {
+      try {
+        const response = await getAll();
+        this.$store.dispatch('addCurrenciesCourses', response)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  },
+  mounted() {
+    this.fetchCurrenciesCourses()
+  }
 };
 </script>
